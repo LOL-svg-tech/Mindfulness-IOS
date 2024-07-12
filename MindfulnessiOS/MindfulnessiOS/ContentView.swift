f3684efd5f22994ed0780b93e876d0a9716236e5
LOL-svg-tech <151696973+LOL-svg-tech@users.noreply.github.com>
2024-07-12 11:02:03 +0800

    1st version of iOS App. not much code yet

---
+++ MindfulnessiOS/MindfulnessiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MindfulnessiOS
+//
+//  Created by Palaniappan Annamalai on 12/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MindfulnessiOS/MindfulnessiOS/MindfulnessiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulnessiOSApp.swift
+//  MindfulnessiOS
+//
+//  Created by Palaniappan Annamalai on 12/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MindfulnessiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
